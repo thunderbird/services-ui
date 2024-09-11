10dc6f1fc63bf8b56a6c572986a9ad0a4c13f3b5
Melissa Autumn <melissa@thunderbird.net>
2024-09-11 15:20:02 -0700

    Add library build settings

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,30 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
 import '@/assets/styles/main.css'
 
-createApp(App).mount('#app')
+// Buttons
+import BaseButton from "@/elements/BaseButton.vue";
+import PrimaryButton from "@/elements/PrimaryButton.vue";
+import SecondaryButton from "@/elements/SecondaryButton.vue";
+import LinkButton from "@/elements/LinkButton.vue";
+
+// Inputs
+import SelectInput from "@/elements/SelectInput.vue";
+import TextInput from "@/elements/TextInput.vue";
+import BubbleSelect from "@/elements/BubbleSelect.vue";
+import SyncCard from "@/elements/SyncCard.vue";
+
+// Misc
+import NoticeBar from "@/elements/NoticeBar.vue";
+import ToolTip from "@/elements/ToolTip.vue";
+
+export {
+  BaseButton,
+  PrimaryButton,
+  SecondaryButton,
+  LinkButton,
+  SelectInput,
+  TextInput,
+  BubbleSelect,
+  SyncCard,
+  NoticeBar,
+  ToolTip
+};
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -12,5 +12,24 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'ServicesUI',
+      fileName: 'services-ui',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
