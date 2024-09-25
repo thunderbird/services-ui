bba8d5ce5694f064adb00776c2c8d33ce6486d50
Melissa Autumn <melissa@thunderbird.net>
2024-09-25 13:58:35 -0700

    Version bump + publish the dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'ServicesUI',
-      fileName: 'services-ui',
+      fileName: 'index',
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
