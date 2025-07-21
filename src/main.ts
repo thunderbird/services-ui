0a17d703a9d36267ea8f5ff2d4296f7a366ca4d0
Andreas Müller <mail@devmount.de>
2025-07-21 17:14:57 +0200

    🔨 Sort exports

--- src/main.ts
+++ src/main.ts
@@ -1,74 +1,78 @@
 import '@/assets/styles/main.css';
 
 // Badges
-import BaseBadge from "@/elements/BaseBadge.vue";
-import PrimaryBadge from "@/elements/PrimaryBadge.vue";
-import SecondaryBadge from "@/elements/SecondaryBadge.vue";
-import WarningBadge from "@/elements/WarningBadge.vue";
-import ExpiryBadge from "@/elements/ExpiryBadge.vue";
+import BaseBadge from '@/elements/BaseBadge.vue';
+import ExpiryBadge from '@/elements/ExpiryBadge.vue';
+import PrimaryBadge from '@/elements/PrimaryBadge.vue';
+import SecondaryBadge from '@/elements/SecondaryBadge.vue';
+import WarningBadge from '@/elements/WarningBadge.vue';
 
 // Buttons
-import BaseButton from "@/elements/BaseButton.vue";
-import PrimaryButton from "@/elements/PrimaryButton.vue";
-import SecondaryButton from "@/elements/SecondaryButton.vue";
-import LinkButton from "@/elements/LinkButton.vue";
+import BaseButton from '@/elements/BaseButton.vue';
+import LinkButton from '@/elements/LinkButton.vue';
+import PrimaryButton from '@/elements/PrimaryButton.vue';
+import SecondaryButton from '@/elements/SecondaryButton.vue';
 
 // Inputs
-import SelectInput from "@/elements/SelectInput.vue";
-import TextInput from "@/elements/TextInput.vue";
-import BubbleSelect from "@/elements/BubbleSelect.vue";
-import SyncCard from "@/elements/SyncCard.vue";
-import SwitchToggle from "@/elements/SwitchToggle.vue";
-import TextArea from '@/elements/TextArea.vue';
+import BubbleSelect from '@/elements/BubbleSelect.vue';
 import CheckboxInput from '@/elements/CheckboxInput.vue';
+import SelectInput from '@/elements/SelectInput.vue';
+import SwitchToggle from '@/elements/SwitchToggle.vue';
+import SyncCard from '@/elements/SyncCard.vue';
+import TextArea from '@/elements/TextArea.vue';
+import TextInput from '@/elements/TextInput.vue';
 
 // Misc
-import NoticeBar from "@/elements/NoticeBar.vue";
-import ToolTip from "@/elements/ToolTip.vue";
+import NoticeBar from '@/elements/NoticeBar.vue';
+import ToolTip from '@/elements/ToolTip.vue';
 
 // Icons
-import CopyIcon from "@/icons/CopyIcon.vue";
-import RefreshIcon from "@/icons/RefreshIcon.vue";
-import NoticeInfoIcon from "@/icons/NoticeInfoIcon.vue";
-import NoticeSuccessIcon from "@/icons/NoticeSuccessIcon.vue";
-import NoticeWarningIcon from "@/icons/NoticeWarningIcon.vue";
-import NoticeCriticalIcon from "@/icons/NoticeCriticalIcon.vue";
-import StatusInfoIcon from './icons/StatusInfoIcon.vue';
-import StatusWarningIcon from './icons/StatusWarningIcon.vue';
-import StatusExpiryIcon from './icons/StatusExpiryIcon.vue';
+import CopyIcon from '@/icons/CopyIcon.vue';
+import NoticeCriticalIcon from '@/icons/NoticeCriticalIcon.vue';
+import NoticeInfoIcon from '@/icons/NoticeInfoIcon.vue';
+import NoticeSuccessIcon from '@/icons/NoticeSuccessIcon.vue';
+import NoticeWarningIcon from '@/icons/NoticeWarningIcon.vue';
+import RefreshIcon from '@/icons/RefreshIcon.vue';
+import StatusExpiryIcon from '@/icons/StatusExpiryIcon.vue';
+import StatusInfoIcon from '@/icons/StatusInfoIcon.vue';
+import StatusWarningIcon from '@/icons/StatusWarningIcon.vue';
 
 // Types
-import { ExpiryUnitTypes, NoticeBarTypes } from "./definitions";
+import { ExpiryUnitTypes, NoticeBarTypes } from '@/definitions';
 
 export {
+  // Badges
   BaseBadge,
+  ExpiryBadge,
   PrimaryBadge,
   SecondaryBadge,
   WarningBadge,
-  CheckboxInput,
-  ExpiryBadge,
+  // Buttons
   BaseButton,
+  LinkButton,
   PrimaryButton,
   SecondaryButton,
-  LinkButton,
-  SelectInput,
-  TextInput,
-  TextArea,
+  // Inputs
   BubbleSelect,
-  SyncCard,
+  CheckboxInput,
+  SelectInput,
   SwitchToggle,
+  SyncCard,
+  TextArea,
+  TextInput,
+  // Misc
   NoticeBar,
   ToolTip,
   // Icons
   CopyIcon,
-  RefreshIcon,
+  NoticeCriticalIcon,
   NoticeInfoIcon,
   NoticeSuccessIcon,
   NoticeWarningIcon,
-  NoticeCriticalIcon,
+  RefreshIcon,
+  StatusExpiryIcon,
   StatusInfoIcon,
   StatusWarningIcon,
-  StatusExpiryIcon,
   // Types
   ExpiryUnitTypes,
   NoticeBarTypes,
