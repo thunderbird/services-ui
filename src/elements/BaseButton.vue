b4065de4d33d6cbc4d574289c8ada290602d16a6
Davi Nakano <davinakanoca@gmail.com>
2025-09-17 12:20:07 -0600

    first pass on updating buttons styles

--- src/elements/BaseButton.vue
+++ src/elements/BaseButton.vue
@@ -4,27 +4,32 @@ import ToolTip from '@/elements/ToolTip.vue';
 
 // component properties
 interface Props {
-  type?: 'primary' | 'secondary' | 'danger' | 'link';
-  size?: 'regular' | 'small';
+  type?: 'primary' | 'brand' | 'destructive' | 'link';
+  size?: 'default' | 'small';
+  variant?: 'filled' | 'outline';
   tooltip?: string;
   forceTooltip?: boolean;
   dataTestid?: string;
+  disabled?: boolean;
 }
 withDefaults(defineProps<Props>(), {
-  type: 'secondary',
-  size: 'regular',
+  type: 'primary',
+  size: 'default',
+  variant: 'outline',
   tooltip: '',
   forceTooltip: false,
   dataTestid: 'button',
+  disabled: false,
 });
 </script>
 
 <template>
   <button
-    class="tbpro-button"
-    :class="{ [type]: type, small: size === 'small' }"
+    class="base"
+    :class="{ [type]: type, small: size === 'small', [variant]: variant }"
     type="button"
     :data-testid="dataTestid"
+    :disabled="disabled"
   >
     <span class="icon" v-if="$slots?.icon">
       <slot name="icon" />
@@ -59,57 +64,171 @@ button:hover > .tooltip,
   opacity: 1;
 }
 
-.primary {
-  --colour-btn-border: var(--colour-service-primary-hover);
+.base {
+  --faded-colour-btn-border: var(--colour-btn-border, var(--colour-neutral-border));
 
-  background-color: var(--colour-service-primary);
-  color: var(--colour-neutral-base);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
 
-  &:hover:enabled {
-    --colour-btn-border: var(--colour-success-pressed);
+  border-radius: var(--border-radius);
+  font-family: 'Inter', 'sans-serif';
+  font-size: var(--txt-input); /* 14px */
+  font-weight: 400;
+  padding: 1rem 1.12rem;
+  cursor: pointer;
 
-    background-color: var(--colour-service-primary-hover);
+  .icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 0.75rem;
+    height: 0.75rem;
+
+    svg {
+      width: 100%;
+      height: 100%;
+    }
   }
 
-  &:active:enabled {
-    --colour-btn-border: var(--colour-service-primary-pressed);
-
-    background-color: var(--colour-service-primary-pressed);
-    box-shadow: none;
-  }
-}
-
-.secondary {
-  background-color: var(--colour-neutral-subtle);
-  color: var(--colour-ti-base);
-
-  &:hover:enabled {
-    box-shadow: 0 0.25rem 0.125rem -0.1875rem #0000001a;
+  &:active {
+    outline: none !important;
   }
 
-  &:active:enabled {
-    --colour-btn-border: var(--colour-neutral-border);
+  &:focus {
+    outline: 0.125rem solid var(--colour-primary-default);
+    outline-offset: 0.125rem;
+  }
 
+  &:disabled {
     background-color: var(--colour-neutral-border);
+    border-color: var(--colour-neutral-border);
+    color: var(--colour-ti-muted);
+    cursor: not-allowed;
   }
 }
 
-.danger {
-  --colour-btn-border: var(--colour-danger-hover);
+.primary {
+  &.filled {
+    background: linear-gradient(180deg, var(--colour-accent-blue) -31.82%, var(--colour-primary-default) 8.74%, var(--colour-primary-hover) 100%);
+    border: none;
+    color: var(--colour-neutral-base);
 
-  background-color: var(--colour-danger-default);
-  color: var(--colour-neutral-base);
+    &:hover:enabled {
+      background: var(--colour-primary-hover);
+    }
 
-  &:hover:enabled {
-    --colour-btn-border: var(--colour-danger-pressed);
+    &:active:enabled {
+      background: var(--colour-primary-pressed);
+    }
 
-    background-color: var(--colour-danger-hover);
+    &:disabled {
+      background: var(--colour-neutral-border);
+    }
   }
 
-  &:active:enabled {
-    --colour-btn-border: var(--colour-danger-pressed);
+  &.outline {
+    border: 1px solid transparent;
+    background:
+      linear-gradient(var(--colour-neutral-base), var(--colour-neutral-base)) padding-box,
+      linear-gradient(180deg, var(--colour-primary-default) 31%, var(--colour-primary-hover) 100%) border-box;
+    color: var(--colour-primary-hover);
 
-    background-color: var(--colour-danger-pressed);
+    &:hover:enabled {
+      border: 2px solid var(--colour-primary-hover);
+      border-image: none;
+      margin: -1px; /* Compensate for the extra 1px border to prevent layout shift */
+    }
+
+    &:active:enabled {
+      background: color-mix(in srgb, var(--colour-accent-blue), transparent 90%);
+    }
+
+    &:disabled {
+      /* TODO: Border colour is not a variable, roughly translates to #ADACB3 */
+      border: 1px solid rgba(119, 117, 128, 0.50);
+      background: var(--colour-neutral-border);
+    }
+  }
+}
+
+.brand {
+  padding: 1rem 1.5rem;
+  font-weight: 600;
+  font-size: 0.8125rem;
+  text-transform: uppercase;
+  border: 1px solid transparent;
+
+  &.filled {
+    background:
+      linear-gradient(329deg, var(--colour-primary-default) -21.06%, var(--colour-accent-blue) 64%) padding-box,
+      /* TODO: This gradient doesn't seem correct and colours are not variables */
+      linear-gradient(#A0E1FF, #2B8CDC) border-box;
+    color: var(--colour-ti-base);
+
+    &:hover:enabled {
+      background:
+        linear-gradient(var(--colour-primary-hover), var(--colour-primary-hover)) padding-box,
+        /* TODO: This gradient doesn't seem correct and colours are not variables */
+        linear-gradient(#A0E1FF, #2B8CDC) border-box;
+      color: var(--colour-neutral-base);
+    }
+
+    &:active:enabled {
+      background:
+        linear-gradient(var(--colour-primary-pressed), var(--colour-primary-pressed)) padding-box,
+        /* TODO: This gradient doesn't seem correct and colours are not variables */
+        linear-gradient(#A0E1FF, #2B8CDC) border-box;
+      color: var(--colour-neutral-base);
+    }
+  }
+
+  &.outline {
+    background:
+      /* TODO: This is light/gray/gray-50 (?) in Figma */
+      linear-gradient(#FFF, #FFF) padding-box,
+      linear-gradient(99deg, var(--colour-accent-blue) 19.15%, var(--colour-accent-gray) 75.77%) border-box;
+
+    &:hover:enabled {
+      background:
+        linear-gradient(var(--colour-neutral-base), var(--colour-neutral-base)) padding-box,
+        linear-gradient(99deg, var(--colour-accent-blue) 19.15%, var(--colour-accent-gray) 75.77%) border-box;
+    }
+  }
+}
+
+.destructive {
+  &.filled {
+    background-color: var(--colour-danger-default);
+    color: var(--colour-neutral-base);
+
+    &:hover:enabled {
+      background-color: var(--colour-danger-hover);
+    }
+
+    &:active:enabled {
+      background-color: var(--colour-danger-pressed);
+    }
+  }
+
+  &.outline {
+    border: 1px solid var(--colour-danger-default);
+    background: var(--colour-neutral-base);
+    color: var(--colour-danger-default);
+
+    &:hover:enabled {
+      border: 2px solid var(--colour-danger-hover);
+      margin: -1px; /* Compensate for the extra 1px border to prevent layout shift */
+      color: var(--colour-danger-hover);
+    }
+
+    &:active:enabled {
+      border: 2px solid var(--colour-danger-pressed);
+      background-color: var(--colour-danger-soft);
+      margin: -1px; /* Compensate for the extra 1px border to prevent layout shift */
+      color: var(--colour-danger-pressed);
+    }
   }
 }
 
@@ -138,75 +257,16 @@ button:hover > .tooltip,
   }
 }
 
-.tbpro-button {
-  --faded-colour-btn-border: var(--colour-btn-border, var(--colour-neutral-border));
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 0.625rem;
-
-  border-radius: var(--border-radius);
-  @mixin faded-border var(--faded-colour-btn-border);
-  box-shadow: 0 0.375rem 0.188rem -0.25rem #152f3c4d;
-  font-family: 'Inter', 'sans-serif';
-  font-size: var(--txt-input); /* 13px */
-  line-height: 0.983rem; /* 15.73px */
-  font-weight: 700;
-
-  transition: var(--transition);
-  cursor: pointer;
-
-  &:hover {
-    box-shadow: 0 0.25rem 0.125rem -0.1875rem #0000001a;
-  }
-
-  &:active {
-    box-shadow: none;
-    outline: none !important;
-  }
-
-  &:focus {
-    outline: 0.125rem solid var(--colour-service-primary);
-    outline-offset: 0.0625rem;
-    box-shadow: none;
-  }
-
-  &:disabled {
-    background-color: var(--colour-neutral-border);
-    border-color: var(--colour-neutral-border);
-    color: var(--colour-ti-muted);
-    box-shadow: none;
-    cursor: not-allowed;
-  }
-}
-
-.icon {
-  display: flex;
-  align-items: center;
-  height: 100%;
-
-  padding-left: 0.75rem;
-  padding-right: 0.75rem;
-  margin-right: -0.75rem;
-}
-
-.text {
-  user-select: none;
-  padding: 0.75rem 1.5rem;
-}
-
 .small {
+  padding: 0.38rem 0.75rem;
+
   .text {
-    text-transform: uppercase;
-    font-size: 0.5625rem;
-    padding: 0.5625rem;
-    font-weight: 700;
+    font-size: 0.875rem;
   }
 
   &button {
     min-width: initial;
-    height: 1.25rem;
+    height: 2rem;
   }
 }
 </style>
