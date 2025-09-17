3b7a3607e642881c173e67a1864e8e04c10c7e62
Davi Nakano <davinakanoca@gmail.com>
2025-09-17 15:08:02 -0600

    remove unnecessary @storybook/addon-onboarding

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,6 @@ import type { StorybookConfig } from '@storybook/vue3-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-themes',
     '@storybook/addon-a11y',
