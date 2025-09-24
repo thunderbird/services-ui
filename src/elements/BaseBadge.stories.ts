af8d89a0dfa7fc92da47d4658597a2392f94069f
Davi Nakano <davinakanoca@gmail.com>
2025-09-24 11:25:39 -0600

    Implement all BaseBadge type variants

--- src/elements/BaseBadge.stories.ts
+++ src/elements/BaseBadge.stories.ts
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/vue3-vite';
 
 import BaseBadge from '@/elements/BaseBadge.vue';
+import CheckCircleFilledIcon from '@/icons/CheckCircleFilledIcon.vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta: Meta<typeof BaseBadge> = {
@@ -9,8 +10,7 @@ const meta: Meta<typeof BaseBadge> = {
   // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   argTypes: {
-    type: { control: 'select', options: ['primary', 'secondary', 'warning'] },
-    icon: { control: 'boolean' },
+    type: { control: 'select', options: ['set', 'primary', 'subscription', 'pending', 'not-set', 'verified', 'emails', 'default'] },
     default: { control: 'text' },
   },
   args: {
@@ -21,48 +21,233 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+export const Set: Story = {
+  args: {
+    type: 'set',
+    default: 'set'
+  },
+};
+
+export const SetWithIcon: Story = {
+  args: {
+    type: 'set',
+    default: 'set'
+  },
+  render: (args) => ({
+    components: { BaseBadge, CheckCircleFilledIcon },
+    setup() {
+      return { args };
+    },
+    template: `
+      <BaseBadge type="set">
+        <template #icon>
+          <CheckCircleFilledIcon />
+        </template>
+        {{ args.default }}
+      </BaseBadge>
+    `,
+  }),
+};
+
+export const Primary: Story = {
+  args: {
+    type: 'primary',
+    default: 'primary'
+  },
+};
+
+export const PrimaryWithIcon: Story = {
+  args: {
+    type: 'primary',
+    default: 'primary'
+  },
+  render: (args) => ({
+    components: { BaseBadge, CheckCircleFilledIcon },
+    setup() {
+      return { args };
+    },
+    template: `
+      <BaseBadge>
+        <template #icon>
+          <CheckCircleFilledIcon />
+        </template>
+        {{ args.default }}
+      </BaseBadge>
+    `,
+  }),
+};
+
+export const Subscription: Story = {
+  args: {
+    type: 'subscription',
+    default: 'subscription'
+  },
+};
+
+export const SubscriptionWithIcon: Story = {
+  args: {
+    type: 'subscription',
+    default: 'subscription'
+  },
+  render: (args) => ({
+    components: { BaseBadge, CheckCircleFilledIcon },
+    setup() {
+      return { args };
+    },
+    template: `
+      <BaseBadge type="subscription">
+        <template #icon>
+          <CheckCircleFilledIcon />
+        </template>
+        {{ args.default }}
+      </BaseBadge>
+    `,
+  }),
+};
+
+export const Pending: Story = {
+  args: {
+    type: 'pending',
+    default: 'pending'
+  },
+};
+
+export const PendingWithIcon: Story = {
+  args: {
+    type: 'pending',
+    default: 'pending'
+  },
+  render: (args) => ({
+    components: { BaseBadge, CheckCircleFilledIcon },
+    setup() {
+      return { args };
+    },
+    template: `
+      <BaseBadge type="pending">
+        <template #icon>
+          <CheckCircleFilledIcon />
+        </template>
+        {{ args.default }}
+      </BaseBadge>
+    `,
+  }),
+};
+
+export const NotSet: Story = {
+  args: {
+    type: 'not-set',
+    default: 'not set'
+  },
+};
+
+export const NotSetWithIcon: Story = {
+  args: {
+    type: 'not-set',
+    default: 'not set'
+  },
+  render: (args) => ({
+    components: { BaseBadge, CheckCircleFilledIcon },
+    setup() {
+      return { args };
+    },
+    template: `
+      <BaseBadge type="not-set">
+        <template #icon>
+          <CheckCircleFilledIcon />
+        </template>
+        {{ args.default }}
+      </BaseBadge>
+    `,
+  }),
+};
+
+export const Verified: Story = {
+  args: {
+    type: 'verified',
+    default: 'verified'
+  },
+};
+
+export const VerifiedWithIcon: Story = {
+  args: {
+    type: 'verified',
+    default: 'verified'
+  },
+  render: (args) => ({
+    components: { BaseBadge, CheckCircleFilledIcon },
+    setup() {
+      return { args };
+    },
+    template: `
+      <BaseBadge type="verified">
+        <template #icon>
+          <CheckCircleFilledIcon />
+        </template>
+        {{ args.default }}
+      </BaseBadge>
+    `,
+  }),
+};
+
+export const Emails: Story = {
+  args: {
+    type: 'emails',
+    default: '#emails'
+  },
+};
+
+export const EmailsWithIcon: Story = {
+  args: {
+    type: 'emails',
+    default: '#emails'
+  },
+  render: (args) => ({
+    components: { BaseBadge, CheckCircleFilledIcon },
+    setup() {
+      return { args };
+    },
+    template: `
+      <BaseBadge type="emails">
+        <template #icon>
+          <CheckCircleFilledIcon />
+        </template>
+        {{ args.default }}
+      </BaseBadge>
+    `,
+  }),
+};
+
 export const Default: Story = {
   args: {
-    type: 'primary',
-    icon: false,
+    type: 'default',
+    default: 'default'
   },
 };
 
-export const Secondary: Story = {
+export const DefaultWithIcon: Story = {
   args: {
-    type: 'secondary',
-    icon: false,
-    default: 'Updated',
+    type: 'default',
+    default: 'default'
   },
+  render: (args) => ({
+    components: { BaseBadge, CheckCircleFilledIcon },
+    setup() {
+      return { args };
+    },
+    template: `
+      <BaseBadge type="default">
+        <template #icon>
+          <CheckCircleFilledIcon />
+        </template>
+        {{ args.default }}
+      </BaseBadge>
+    `,
+  }),
 };
 
-export const Warning: Story = {
+export const Counter: Story = {
   args: {
-    type: 'warning',
-    icon: false,
-    default: 'Experimental',
-  },
-};
-
-export const IconPrimary: Story = {
-  args: {
-    type: 'primary',
-    icon: true,
-  },
-};
-
-export const IconSecondary: Story = {
-  args: {
-    type: 'secondary',
-    icon: true,
-    default: 'Updated',
-  },
-};
-
-export const IconWarning: Story = {
-  args: {
-    type: 'warning',
-    icon: true,
-    default: 'Experimental',
+    type: 'counter',
+    default: '2'
   },
 };
---
+++ src/icons/CheckCircleFilledIcon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { t } from '@/composable/i18n';
+</script>
+
+<template>
+  <svg
+    :aria-label="t('icons.check')"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    viewBox="0 0 256 256"
+  >
+    <path
+      d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm45.66,85.66-56,56a8,8,0,0,1-11.32,0l-24-24a8,8,0,0,1,11.32-11.32L112,148.69l50.34-50.35a8,8,0,0,1,11.32,11.32Z">
+    </path>
+  </svg>
+</template>
