2bb58bb616074e2fc5532fe6bf95955539cbddb8
Andreas Müller <mail@devmount.de>
2025-10-06 16:38:59 +0200

    ➕ Add icon button

---
+++ src/elements/IconButton.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import BaseButton from '@/elements/BaseButton.vue';
+</script>
+
+<template>
+  <base-button type="link" class="icon-only">
+    <template #iconLeft>
+      <slot />
+    </template>
+  </base-button>
+</template>
+
+<style>
+.link.icon-only {
+  padding: .5rem;
+
+  .icon {
+    width: 1em;
+    height: 1em;
+  }
+
+  .text {
+    display: none;
+  }
+}
+</style>
