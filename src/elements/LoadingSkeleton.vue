7dd22bb7023aa73deeed553ff38ff912772b1295
Melissa Autumn <melissa@thunderbird.net>
2025-10-24 11:22:58 -0700

    Add a11y loading message

--- src/elements/LoadingSkeleton.vue
+++ src/elements/LoadingSkeleton.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { computed } from 'vue';
 import { t } from '@/composable/i18n';
 
 interface Props {
@@ -20,25 +19,31 @@ withDefaults(defineProps<Props>(), {
 <template>
   <div :class="{ skeleton: isLoading }" :style="{ width, height, borderRadius }">
     <div :class="{ 'skeleton__hide-content': isLoading }">
+      <span class="skeleton__a11y-message" v-if="isLoading">{{ t('loadingSkeleton.loadingMessage') }}</span>
       <slot></slot>
     </div>
   </div>
 </template>
 
 <style scoped>
+/* via https://adrianroselli.com/2020/11/more-accessible-skeletons.html */
+.skeleton__a11y-message {
+  position: absolute;
+  top: auto;
+  overflow: hidden;
+  clip: rect(1px, 1px, 1px, 1px);
+  width: 1px;
+  height: 1px;
+  white-space: nowrap;
+}
 .skeleton__hide-content {
   visibility: hidden;
 }
 .skeleton {
-  background-color: var(--colour-ti-base-dark);
-  background: linear-gradient(
-    90deg,
-    var(--colour-ti-base-dark) 25%,
-    var(--colour-ti-muted) 50%,
-    var(--colour-ti-base-dark) 75%
-  );
-
+  background: var(--colour-ti-base-dark)
+    linear-gradient(90deg, var(--colour-ti-base-dark) 25%, var(--colour-ti-muted) 50%, var(--colour-ti-base-dark) 75%);
   background-size: 1000% 1000%;
+  cursor: progress;
 
   @media (prefers-reduced-motion: no-preference) {
     animation: loading-anim 6s ease infinite;
