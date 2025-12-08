ddecbae53cc1570f8eccddf6e3bfcc21291c292c
Andreas Müller <mail@devmount.de>
2025-12-08 09:10:29 +0100

    🔨 Clean up

--- src/components/BubbleSelect.stories.ts
+++ src/components/BubbleSelect.stories.ts
@@ -2,7 +2,7 @@ import { fn } from 'storybook/test';
 import type { Meta, StoryObj } from '@storybook/vue3-vite';
 
 import BubbleSelect from '@/components/BubbleSelect.vue';
-import type { SelectOption } from '@/models.ts';
+import type { SelectOption } from '@/models';
 
 const scheduleDayOptions: SelectOption<string>[] = [
   {
