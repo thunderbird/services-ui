e51b488a8e3f5cbe4ff85a0d30c747ef8eefb076
Chris Aquino <chris@thunderbird.net>
2026-03-04 12:48:22 -0500

    Removes unnecessary shim for process.env.NODE_ENV

--- src/index.ce.ts
+++ src/index.ce.ts
@@ -1,6 +1,3 @@
-if (typeof (window as any).process === 'undefined') {
-  (window as any).process = { env: { NODE_ENV: 'production' } };
-}
 import { defineCustomElement } from 'vue';
 
 // Import the Components to convert.
