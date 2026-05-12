40b1c3752df32502cad2af98bc66a230322989e3
Andreas Müller <mail@devmount.de>
2026-05-12 22:08:30 +0200

    ➕ Add input value on blur for SelectInput

--- src/components/SelectInput.vue
+++ src/components/SelectInput.vue
@@ -72,7 +72,7 @@ const onBlur = (evt: ElementEvent<HTMLSelectElement>) => {
     validationMessage.value = '';
   }
 
-  emit('blur');
+  emit('blur', model.value);
 };
 
 defineExpose({ focus, reset });
