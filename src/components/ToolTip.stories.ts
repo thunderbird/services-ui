5ee94665d2a02d1e12c40e6369d98c14877c17bf
Andreas Müller <mail@devmount.de>
2026-05-18 08:33:37 +0200

    ➕ Add story for contextual usage of ToolTips

--- src/components/ToolTip.stories.ts
+++ src/components/ToolTip.stories.ts
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/vue3-vite';
 import { TooltipPosition } from '@/definitions';
 import ToolTip from '@/components/ToolTip.vue';
+import PrimaryButton from './PrimaryButton.vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta: Meta<typeof ToolTip> = {
@@ -51,6 +52,28 @@ export const Position: Story = {
     docs: {
       source: {
         code: '<tool-tip position="pos-top">Pointing upwards</tool-tip>\n<tool-tip position="pos-left">Pointing left</tool-tip>\n<tool-tip position="pos-bottom">Pointing downwards</tool-tip>\n<tool-tip position="pos-right">Pointing right</tool-tip>\n<tool-tip position="pos-none">Pointing nowhere</tool-tip>',
+      },
+    },
+  },
+};
+
+export const Context: Story = {
+  render: () => ({
+    components: { ToolTip, PrimaryButton },
+    template: `<div style="position:relative; padding-top:3rem;">
+      <primary-button>
+        Copy the booking link
+      </primary-button>
+      <tool-tip style="top:0;">
+        This button copies to clipboard
+      </tool-tip>
+      <p>Tooltips are currently not assigned to a specific element, but placed and positioned separately. This will be improved in the future.</p>
+    </div>`,
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: '<div style="position: relative; padding-top: 3rem;">\n  <primary-button>\n    Copy the booking link\n  </primary-button>\n  <tool-tip style="top: 0;">\n    This button copies to clipboard\n  </tool-tip>\n</div>',
       },
     },
   },
